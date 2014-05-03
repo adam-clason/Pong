1275601e4efe49767a3347217a3e786109cef535
Adam Clason <adam.clason@outlook.com>
2014-05-02 22:05:51 -0400

    Trying to get less figured out

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use(function(req, res) {
-    res.sendfile(__dirname, '/public/index.html');
+    res.sendfile(__dirname + '/public/index.html');
 });
 
 
